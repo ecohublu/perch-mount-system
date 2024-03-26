c0d360aafc3e7c71d3bf58a171a52304757d0c35
Tatsutomo <raymond96383@gmail.com>
2024-03-27 00:47:48 +0800

    feat: export history class in model.py and flask db migrate

--- src/model.py
+++ src/model.py
@@ -250,3 +250,10 @@ class ScheduleDetect(db.Model, JsonAbleModel):
     detect_datetime = db.Column(db.DateTime, default=datetime.now)
     section = db.Column(db.Integer, db.ForeignKey("sections.section_id"))
     is_image = db.Column(db.Boolean, default=True)
+
+
+class ExportHistory(db.Model, JsonAbleModel):
+    __tablename__ = "export_history"
+    export_history_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    exportor = db.Column(db.Integer, db.ForeignKey("members.member_id"))
+    file_name = db.Column(db.String(50), nullable=False, unique=True)
