aa7b5955736a6f965f7e5491a43f6557fa8e8571
Tatsutomo <raymond96383@gmail.com>
2024-04-09 00:36:41 +0800

    TODO

--- summary/filer/csv.py
+++ summary/filer/csv.py
@@ -1,7 +1,31 @@
 import datetime
-import io
 import uuid
 import summary.filer
+
+# TODO
+TYPES_MAP = {
+    "name": str,
+    "perch_mount_name": str,
+    "habitat": str,
+    "latitude": str,
+    "longitude": str,
+    "layer": None,
+    "camera": 1,
+    "medium_datetime": datetime.datetime(2022, 5, 11, 11, 54, 17),
+    "prey": False,
+    "prey_name": None,
+    "ring_number": None,
+    "xmax": 0.999772,
+    "xmin": 0.654112,
+    "ymax": 0.95013,
+    "ymin": 0.399243,
+    "chinese_common_name_by_ai": "黑翅鳶",
+    "scientific_name_by_ai": "Elanus caeruleus",
+    "taxon_order_by_ai": 7575,
+    "chinese_common_name_by_human": "黑翅鳶",
+    "scientific_name_by_human": "Elanus caeruleus",
+    "taxon_order_by_human": 7575,
+}
 
 
 class PerchMountCsvData(summary.filer.PerchMounData):
