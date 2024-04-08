760c946c854fdd0eaf037d65bd88eef61a997833
Tatsutomo <raymond96383@gmail.com>
2024-04-08 23:19:56 +0800

    changes

--- service/export_history.py
+++ service/export_history.py
@@ -1,3 +1,19 @@
+import datetime
 import service
 from service import query_utils
 from src import model
+
+
+def get_export_histories_by_exportor(exportor: int) -> model.ExportHistory:
+    with service.session.begin() as session:
+        result = (
+            session.query(model.ExportHistory)
+            .filter(model.ExportHistory.exportor == exportor)
+            .filter(model.ExportHistory.expire_time < datetime.datetime.now())
+            .all()
+        )
+    return result
+
+
+def add_export_history():
+    return
