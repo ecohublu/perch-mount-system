53c0950349cb46ab0265fbea2cf54cc3ba3c820c
Chendada-8474 <raymond96383@gmail.com>
2024-04-14 21:31:54 +0800

    remove tmp minio user name

--- src/s3.py
+++ src/s3.py
@@ -6,8 +6,7 @@ URL = urllib.parse.urlparse(config.get_env(config.EnvKeys.MINIO))
 
 client = Minio(
     URL.netloc,
-    # access_key=config.get_env(config.EnvKeys.MINIO_ACCESS_KEY),
-    access_key="ROOTNAME",
+    access_key=config.get_env(config.EnvKeys.MINIO_ACCESS_KEY),
     secret_key=config.get_env(config.EnvKeys.MINIO_SECRET_KEY),
     secure=False,
 )
