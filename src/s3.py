7543bd880cb1d78c04693e1fb564989f79558033
Chendada-8474 <raymond96383@gmail.com>
2024-04-14 22:25:03 +0800

    style: get minio access var in global

--- src/s3.py
+++ src/s3.py
@@ -3,10 +3,12 @@ from minio import Minio
 from src import config
 
 URL = urllib.parse.urlparse(config.get_env(config.EnvKeys.MINIO))
+ACCESS_KEY = config.get_env(config.EnvKeys.MINIO_ACCESS_KEY)
+SECRET_KEY = config.get_env(config.EnvKeys.MINIO_SECRET_KEY)
 
 client = Minio(
     URL.netloc,
-    access_key=config.get_env(config.EnvKeys.MINIO_ACCESS_KEY),
-    secret_key=config.get_env(config.EnvKeys.MINIO_SECRET_KEY),
+    access_key=ACCESS_KEY,
+    secret_key=SECRET_KEY,
     secure=False,
 )
